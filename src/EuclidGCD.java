da8dcd2b035108f2934979ecf27abd05f59940d3
WOO_SUNG <83216048+NOEL-code@users.noreply.github.com>
2024-01-19 08:00:16 +0900

    no message

--- src/EuclidGCD.java
+++ src/EuclidGCD.java
@@ -7,7 +7,7 @@ public class EuclidGCD {
             return x;
         }
         else {
-            return gcd(y, x%y);
+            return gcd(y, x % y);
         }
     }
     public static void main(String[] args) {
---
+++ src/Hanoi.java
@@ -0,0 +1,5 @@
+public class Hanoi {
+    public static void main(String[] args) {
+
+    }
+}
