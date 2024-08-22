1cad5bd531b67a44fd558fa6f98a6a988e7fc823
NOEL-code <junusong12@naver.com>
2024-08-22 21:31:17 +0200

    p149

---
+++ python/part5/5-8.py
@@ -0,0 +1,49 @@
+from collections import deque
+
+def bfs(graph, start, visited):
+    # 큐 라이브러리
+    queue = deque([start])
+    # 현재 노드 방문 처리
+    visited[start] = True
+    # 큐가 빌 때까지 반복
+    while queue:
+        # 큐에서 하나의 원소를 뽑아 출력
+        v = queue.popleft()
+        print(v, end=' ')
+        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+# BFS 수행
+visited = [False] * 9
+print("BFS: ", end="")
+bfs(graph, 1, visited)
+print()
+
+# DFS 수행
+visited = [False] * 9
+print("DFS: ", end="")
+dfs(graph, 1, visited)
+print()
