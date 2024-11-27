268aa1a6b2c1b9438647a978b1adfe800c887367
NOEL-code <junusong12@naver.com>
2024-11-27 11:01:24 +0100

    코테

--- python/p298.py
+++ python/p298.py
@@ -1,44 +1,31 @@
-def find_team(team, x):
-  if team[x] != x:
-    team[x] = find_team(team, team[x])
-  return team[x]
+n , m = map(int, input().split())
 
-  
-def union_team(team, a, b):
-  
-  a = find_team(team, a)
-  b = find_team(team, b)
-  
+parent = [0] * (n + 1)
+
+for i in range(n + 1):
+  parent[i] = i
+
+def find_parent(parent, x):
+  if parent[x] != x:
+    find_parent(parent, parent[x])
+  return parent[x]
+                
+def union_parent(parent, a, b):
+  a = find_parent(parent, a)
+  b = find_parent(parent, b)
   if a < b:
-    team[b] = a
+    parent[b] = a
   else:
-    team[a] = b
-    
-def check_same_team(team, a, b):
-  if find_team(team, a) == find_team(team, b):
-    return True
-  else:
-    return False
-
-n, m = map(int, input().split())
-  
-team = [0] * (n + 1)
-
-for i in range(1, n + 1):
-  team[i] = i
-  
+    parent[a] = b
+      
+              
 for _ in range(m):
-  operation, a, b = map(int, input().split())
+  oper, a, b = map(int, input().split())
   
-  if operation == 0:
-    if check_same_team(team, a, b):
-      continue
-    else:
-      union_team(team, a, b)
-    
-  else:
-    if check_same_team(team, a, b):
+  if oper == 1:
+    if find_parent(parent, a) == find_parent(parent, b):
       print("YES")
     else:
       print("NO")
-
+  else:
+    union_parent(a, b)
