5aab0a06aeb7a6e2da2a4609c2fa5cff081db38d
NOEL-code <junusong12@naver.com>
2024-12-04 19:14:25 +0100

    코테

--- python/pratice.py
+++ python/pratice.py
@@ -1,4 +1,15 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 
-for i in range(1, n + 1):
-    print("*" * i + " " * (n - i) + " " * (n - i)+ "*" * (i))
+data = []
+for i in range(n):
+    age, name = input().split()
+    data.append([int(age), name])
+
+s_array = sorted(data, key=lambda x: x[0])
+
+for i in range(n):
+    print(s_array[i][0], s_array[i][1])
+
--- python/tempCodeRunnerFile.py
+++ python/tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-n = int(input())
 
-
-for i in range(n, -1, 0):
-    print("*" * i)
