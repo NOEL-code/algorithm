ed275f2e88e39ccfb43a3151ef01cf0fe5535fa0
NOEL-code <junusong12@naver.com>
2024-12-05 10:28:27 +0100

    코테

--- python/pratice.py
+++ python/pratice.py
@@ -1,15 +1,10 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
+s = input().strip()
 
-data = []
-for i in range(n):
-    age, name = input().split()
-    data.append([int(age), name])
+data = [0] * 26  # 알파벳 26자에 대한 배열 생성
 
-s_array = sorted(data, key=lambda x: x[0])
-
-for i in range(n):
-    print(s_array[i][0], s_array[i][1])
-
+for char in s:
+    if 'a' <= char <= 'z':  # 소문자 알파벳만 처리
+        data[ord(char) - 97] += 1
--- python/tempCodeRunnerFile.py
+++ python/tempCodeRunnerFile.py
@@ -1,1 +1,13 @@
+import sys
+input = sys.stdin.readline
 
+n = int(input())
+
+start = 1 
+
+for i in range(0, n):
+    start = start + (6 * i)
+    
+    if start > n:
+        print(i + 1)
+        break
