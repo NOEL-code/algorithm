2ee34493ff80a3987cee172b6de4ec2f6a068398
NOEL-code <junusong12@naver.com>
2024-12-13 18:34:17 +0100

    ㅍㅋ

--- python/pratice.py
+++ python/pratice.py
@@ -1,26 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input().strip())
+s = input().strip()
 
-d = [0] * 41
-zero = [0] * 41
-one = [0] * 41
+data = ["ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"]
+result = 0
 
-# 초기값 설정
-d[0], d[1] = 0, 1
-zero[0], one[0] = 1, 0
-zero[1], one[1] = 0, 1
 
-for i in range(2, 41):
-    d[i] = d[i - 1]+ d[i - 2]
-    zero[i] = zero[i - 1]+ zero[i - 2]
-    one[i] = one[i - 1]+ one[i - 2]
-    
-for i in range(n):
-    
-    x = int(input())
-    
-    print(zero[x], one[x])
-    
-    
+for i in range(len(s)):
+    for j in data:
+        if s[i] in j:
+            result += data.index(j) + 3
+
+print(result)
--- python/tempCodeRunnerFile.py
+++ python/tempCodeRunnerFile.py
@@ -1,46 +1,9 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input().strip())
+n = int(input())
 
-d = [0] * 41
-zero = [0] * 41
-one = [0] * 41
-
-zero[0] = 1
-zero[1] = 0
-zero[2] = 1
-zero[3] = 1
-
-one[0] = 0
-one[1] = 1
-one[2] = 1
-one[3] = 2
-
-def fibo(x):
-
-    if x == 0:
-        return 0
-    elif x == 1:
-        return 1
-    else:
-        if d[x] != 0:
-            return d[x]
-        else:
-            d[x] = fibo(x - 1) + fibo(x - 2)
-            zero[x] = zero[x - 1] + zero[x - 2]
-            one[x] = one[x - 1] + one[x - 2]
-            return d[x]
-        
 for i in range(n):
-
-    x = int(input())
+    s = input().strip()
     
-    result = fibo(x)
-    
-    print(zero[x], one[x])
-    
-    zero = [0] * 41
-    one = [0] * 41
-    
-    
+    print(s[0], s[len(s) -1])
