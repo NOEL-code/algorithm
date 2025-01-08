c0d298c3f4ca62b35481cd4f732e8ddc18b9871c
NOEL-code <junusong12@naver.com>
2025-01-08 21:34:48 +0200

    ㅋㅌ

--- python/백준/자료구조/덱, 큐/1021 회전하는 큐.py
+++ python/백준/자료구조/덱, 큐/1021 회전하는 큐.py
@@ -1,4 +1,23 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-N, M = map(int, input()
+N, M = map(int, input().split())
+
+numbers = list(map(int, input().split()))
+dequeue = deque([i for i in range(1, N + 1)])
+
+count = 0
+while(numbers):
+    if dequeue[0] == numbers[0]:
+        dequeue.popleft()
+        numbers.pop(0)
+    else:
+        if dequeue.index(numbers[0]) <= len(dequeue) // 2:
+            dequeue.rotate(-1)
+            count += 1
+        else:
+            dequeue.rotate(1)
+            count += 1
+        
+print(count)
---
+++ python/백준/재귀/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+n = int(input())
+count = 0
+def hanoi(n, start, end, via):
+    if n == 1:
+        print(start, end)
+        return
+    hanoi(n - 1, start, via, end)
+    print(start, end)
+    hanoi(n - 1, via, end, start)
+print(2**n -1)
+hanoi(n, 1, 3, 2)
