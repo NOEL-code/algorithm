6a8af158b06704402e1e6d6eb3b299bef8aa2f66
NOEL-code <junusong12@naver.com>
2025-01-12 15:52:26 +0100

    Update 15649 N과 M (1).py

--- python/백준/백트래킹/15649 N과 M (1).py
+++ python/백준/백트래킹/15649 N과 M (1).py
@@ -1,4 +1,21 @@
+def back_tracking(arr, now_length, used_num):
+    if now_length == M:
+        print(*arr)
+        return
+    
+    for idx in range(1, N + 1):
+        if not used_num[idx]:
+            arr.append(idx)
+            used_num[idx] = True
+            back_tracking(arr, now_length + 1, used_num)
+            arr.pop()
+            used_num[idx] = False
+
 N, M = map(int, input().split())
 
-for i in range(N):
-    for j in range(M):
+num = [i + 1 for i in range(N + 1)]
+
+used_num = [False] * (N + 1)
+
+back_tracking([], 0, used_num)
+
