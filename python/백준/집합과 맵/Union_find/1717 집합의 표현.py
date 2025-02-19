41722214b234bb3a9e57b8a4bfbc0815fcf697c0
NOEL-code <junusong12@naver.com>
2025-02-19 11:01:33 +0900

    Create 1717 집합의 표현.py

---
+++ python/백준/집합과 맵/Union_find/1717 집합의 표현.py
@@ -0,0 +1,36 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+parent = [0] * (n + 1)
+
+for i in range(n + 1):
+    parent[i] = i
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+        
+    
+for _ in range(m):
+    oper, a, b = map(int, input().split())
+    
+    if oper == 0:
+        union_parent(parent, a, b)
+    else:
+        if find_parent(parent, a) == find_parent(parent, b):
+            print("YES")
+        else:
+            print("NO")
+    
