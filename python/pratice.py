e8f5898f2e1405582ee784fdb5d0f0b6e4bfecb4
NOEL-code <junusong12@naver.com>
2025-02-25 16:36:31 +0900

    ㅋㅌ

--- python/pratice.py
+++ python/pratice.py
@@ -1,38 +1,40 @@
-def find_parent(parent, x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
-    return parent[x]
+import sys
+from collections import deque
 
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
+input = sys.stdin.readline
+
+T = int(input())
+
+def bfs(x, y, graph, visited, M, N):
+    directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    queue = deque([x, y])
+    visited[y][x] = True
     
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
-
-v, e = map(int, input().split())
-
-parent = [0] * (v + 1)
-
-edges = []
-result = 0
-
-for i in range(1, v + 1):
-    parent[i] = i
-
-for _ in range(e):
-    a, b, cost = map(int, input().split())
-    edges.append((cost, a, b))
-    
-edges.sort()
-
-for edge in edges:
-    cost, a, b = edge
-    
-    if find_parent(parent, a) != find_parent(parent, b):
-        union_parent(parent, a, b)
-        result += cost
+    while queue:
+        x, y = queue.popleft()
         
-print(result)
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < M and 0 <= ny < N:
+                if not visited[ny][nx] and graph[ny][nx] == -1:
+                    visited[ny][nx] = True
+                    queue.append((nx, ny))
+
+for _ in range(T):
+    M, N, K = map(int, input().split())
+    
+    graph = [[0] * M for _ in range(N)]
+    visited = [[False] * M for _ in range(N)]
+    
+    for _ in range(K):
+        x, y = map(int, input().split())
+        graph[y][x] = 1
+        
+    count = 0
+    for i in range(N):
+        for j in range(M):
+            if graph[i][j] == 1 and not visited[i][j]:
+                bfs(j, i, graph, visited, M, N)
+                count += 1
+                
+    print(count)
