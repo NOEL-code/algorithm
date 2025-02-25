0eca379ee6576f5a2dfa261cd249e4a37c8f418c
NOEL-code <junusong12@naver.com>
2025-02-25 22:18:52 +0900

    ㅋㅌ

--- python/백준/다이나믹 프로그래밍/1149 RGB거리.py
+++ python/백준/다이나믹 프로그래밍/1149 RGB거리.py
@@ -5,13 +5,13 @@ input = sys.stdin.readline
 N = int(input())
 
 graph = [list(map(int, input().rstrip().split())) for _ in range(N)]
-d = [[0] * 3 for _ in range(N)]
+dp = [[0] * 3 for _ in range(N)]
 
-d[0] = graph[0]
-    
+dp[0] = graph[0]
+
 for i in range(1, N):
-    d[i][0] = min(d[i - 1][1], d[i - 1][2]) + graph[i][0]
-    d[i][1] = min(d[i - 1][0], d[i - 1][2]) + graph[i][1]
-    d[i][2] = min(d[i - 1][1], d[i - 1][0]) + graph[i][2]
-    
-print(min(d[N - 1]))
+    dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + graph[i][0]
+    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + graph[i][1]
+    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + graph[i][2]
+
+print(min(dp[N-1]))
