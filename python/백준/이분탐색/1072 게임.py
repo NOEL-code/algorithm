1f8413acce88e8fe43e2362609dd0bf33568f404
NOEL-code <junusong12@naver.com>
2025-03-25 08:58:00 +0900

    ㅋㅌ

---
+++ python/백준/이분탐색/1072 게임.py
@@ -0,0 +1,22 @@
+X, Y = map(int, input().split())
+Z = int((Y / X) * 100)
+
+def search():
+    start = 1
+    end = int(1e9)
+    min_value = int(1e9)
+    
+    while start <= end:        
+        mid = (start + end) // 2
+        percent = int((Y + mid) / (X + mid) * 100)
+        if percent > Z:
+            end = mid - 1     
+            min_value = mid  
+        else:
+            start = mid + 1
+    return min_value
+
+if Z >= 99:
+    print(-1)
+else:
+    print(search())
