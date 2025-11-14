4f8f094163e6a29fab85ccd9ae46e6ed47010035
WOO_SUNG <junusong12@naver.com>
2025-11-14 09:11:03 +0900

    Update 2467 용액.py

--- python/백준/이분탐색/2467 용액.py
+++ python/백준/이분탐색/2467 용액.py
@@ -1,29 +1,28 @@
-import sys
-
-input = sys.stdin.readline
-
-N = int(input().strip())
+N = int(input())
 nums = list(map(int, input().split()))
+nums.sort()
 
-left, right = 0, N - 1
-best_sum = float('inf')
-a, b = nums[left], nums[right]
+min_num = [0, 0, int(1e9)]  # [값1, 값2, 현재 최솟값(절댓값)]
 
-while left < right:
-    s = nums[left] + nums[right]
+def search(left, right):
+    while left < right:
+        s = nums[left] + nums[right]
 
-    if abs(s) < abs(best_sum):
-        best_sum = s
-        a, b = nums[left], nums[right]
-        if best_sum == 0:  # 더 이상 좋아질 수 없음
-            break
+        # 최솟값 갱신 (절댓값 기준)
+        if abs(s) < abs(min_num[2]):
+            min_num[0], min_num[1], min_num[2] = nums[left], nums[right], abs(s)
 
-    if s > 0:
-        right -= 1
-    else:
-        left += 1
+        if s == 0:
+            print(nums[left], nums[right])
+            return True  # 정확히 0이면 바로 종료
 
-# 오름차순 출력
-if a > b:
-    a, b = b, a
-print(a, b)
+        if s > 0:
+            right -= 1
+        else:
+            left += 1
+    return False
+
+# 이 문제는 한 번만 돌리면 됨
+found = search(0, N-1)
+if not found:
+    print(min_num[0], min_num[1])
