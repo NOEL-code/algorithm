747d51159bdced07262aa96cce7f1183a4ce6b77
WOO_SUNG <junusong12@naver.com>
2025-12-02 23:16:29 +0900

    Update pratice.py

--- pratice.py
+++ pratice.py
@@ -1,31 +1,5 @@
-isbn = input()
-
-check = 0
-
-star_1 = False
-star_3 = False
-
-check = 0
-for i in range(1, 13):
-    if isbn[i] == '*':
-        if i % 2 == 0:
-            star_3 = True
-        else:
-            star_1 = True
-    else:
-        if i % 2 == 0:
-            check += int(isbn[i]) * 3
-        else:
-            check += int(isbn[i])
-
-for i in range(1, 10):
-    if star_3:
-        check += i * 3
-    else:
-        check += i
-
-    r = check % 10
-
-    if (10 - r) % 10 == isbn[12]:
-        print(i)
-        exit(0)
+print("       _.-;;-._")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-;;-._|")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-''-._|")
