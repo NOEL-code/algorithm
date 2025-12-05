2fbf6fc064534ff04173067965361c0f4d15ff77
WOO_SUNG <junusong12@naver.com>
2025-12-05 21:42:16 +0900

    Optimize common subsequence extraction logic
    Refactored the algorithm to efficiently find the largest common subsequence between two lists by iterating from the largest possible value and using pointers for both lists. Removed the use of combinations and simplified the output to print the length and elements of the found subsequence. Also added a new empty Python file.

--- python/백준/다이나믹 프로그래밍/30805 사전 순 최대 공통 부분 수열.py
+++ python/백준/다이나믹 프로그래밍/30805 사전 순 최대 공통 부분 수열.py
@@ -1,17 +1,56 @@
-from itertools import combinations
+import sys
+input = sys.stdin.readline
 
 N = int(input())
 A = list(map(int, input().split()))
-
 M = int(input())
 B = list(map(int, input().split()))
 
-common = []
+# 현재 A, B에서 어디까지 썼는지 가리키는 포인터
+ia = 0
+ib = 0
+ans = []
 
-for a in A:
-    if a in B:
-        common.append(a)
+# 숫자 범위가 1~100
+MAX_VAL = 100
 
-for i in range(1, len(common) + 1):
-    for x in combinations(common, i):
-        print(x)
+while ia < N and ib < M:
+    chosen = None
+    next_ia = next_ib = None
+
+    # 이번에 뽑을 값 v를 큰 것부터 내려가며 탐색
+    for v in range(MAX_VAL, 0, -1):
+        # A에서 v가 ia 이후에 처음 나오는 위치 찾기
+        pa = -1
+        for i in range(ia, N):
+            if A[i] == v:
+                pa = i
+                break
+        if pa == -1:
+            continue  # A에는 더 이상 v가 없음
+
+        # B에서도 v가 ib 이후에 처음 나오는 위치 찾기
+        pb = -1
+        for j in range(ib, M):
+            if B[j] == v:
+                pb = j
+                break
+        if pb == -1:
+            continue  # B에는 더 이상 v가 없음
+
+        # A, B 둘 다에서 ia/ib 이후에 v를 찾았으니, 이 v를 선택
+        chosen = v
+        next_ia = pa + 1
+        next_ib = pb + 1
+        break  # 가장 큰 v를 찾았으니 더 내려갈 필요 없음
+
+    if chosen is None:  # 더 이상 공통으로 뽑을 수 있는 수가 없음
+        break
+
+    ans.append(chosen)
+    ia = next_ia
+    ib = next_ib
+
+print(len(ans))
+if ans:
+    print(*ans)
